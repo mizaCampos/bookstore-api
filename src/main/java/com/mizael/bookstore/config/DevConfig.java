08d12f81ced95db3b2eb4714e97189f2fd6b0bdd
mizaCampos <mizaellauriano276@gmail.com>
2022-10-20 18:34:21 -0300

    criacao do perfil dev

---
+++ src/main/java/com/mizael/bookstore/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.mizael.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mizael.bookstore.service.Dbservice;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private Dbservice dbservice;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciaBaseDeDados(){
+        if(strategy.equals("create")){
+            this.dbservice.instanciaBaseDeDados();
+        }
+        return false;
+    }
+
+
+    
+    
+}
