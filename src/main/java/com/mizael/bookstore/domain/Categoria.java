2f2ec6e5d9e927611fad6de6e5a2277b99b652d7
mizaCampos <mizaellauriano276@gmail.com>
2022-10-20 10:30:37 -0300

    finalizando classes domain

--- src/main/java/com/mizael/bookstore/domain/Categoria.java
+++ src/main/java/com/mizael/bookstore/domain/Categoria.java
@@ -1,20 +1,32 @@
 package com.mizael.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Data
+@Entity
+public class Categoria implements Serializable{
 
-public class Categoria {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     
